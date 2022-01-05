5b21952641c92c54b25b1784799b2808f36c8fc2
Samuel Vieira <smlpeixotovieira@icloud.com>
2022-01-05 08:18:33 -0300

    Muda Title e Description dinamicamente

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Samuel Vieira</title>
+  <title><?php bloginfo('name'); ?></title>
+  <meta name="description" content="<?php bloginfo('description'); ?>">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
