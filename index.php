002f2d1d36e3b1619bb4c1d0bb85ba194510f6bc
Samuel Vieira <smlpeixotovieira@icloud.com>
2022-01-05 07:57:34 -0300

    Fix endereço CSS

--- index.php
+++ index.php
@@ -1,1 +1,183 @@
-Olá
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Samuel Vieira</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+
+  <link rel="icon" type="image/png" href="../img/favicon.png" />
+</head>
+
+<body>
+  <header>
+    <nav>
+      <div class="navbar container">
+        <div class="texto-branding cor-c9">Samuel Vieira</div>
+        <div class="navbar-menu">
+          <ul class="texto-m">
+            <li><a href="#experiencias">Experiência</a></li>
+            <li><a href="#formacao">Formação</a></li>
+            <li><a href="#contato">Contato</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <section class="hero">
+    <div class="hero container" id="perfil">
+      <div class="hero-perfil">
+        <img src="./img/perfil-sq.jpg" alt="Samuel Vieira">
+      </div>
+      <div class="hero-info">
+        <div class="hero-info-icons">
+          <a href="http://instagra.com/speixoto_" target="_blank"><img src="./img/instagram.svg" alt="instagram" class="hero-info-icon"></a>
+          <a href="https://www.linkedin.com/in/samuel-p-vieira/" target="_blank"><img src="./img/linkedin.svg" alt="linkedin" class="hero-info-icon"></a>
+          <a href="https://twitter.com/speixoto_" target="_blank"><img src="./img/twitter.svg" alt="Twiter" class="hero-info-icon"></a>
+          <a href="https://github.com/smkvieira" target="_blank"><img src="./img/github.svg" alt="github" class="hero-info-icon"></a>
+          <a href="https://dribbble.com/smkvieira" target="_blank"><img src="./img/dribble.svg" alt="dribble" class="hero-info-icon"></a>
+          <a href="https://www.behance.net/speixoto_" target="_blank"><img src="./img/behance.svg" alt="behance" class="hero-info-icon"></a>
+        </div>
+        <div class="hero-info-main">
+          <h1 class="texto-xxg cor-c9">Analista de SEO & Estudante Front End</h1>
+          <span class="texto-g cor-c7">São Paulo</span>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="experiencias" class="padding">
+    <div class="experiencias container">
+      <div class="section-title">
+        <h2 class="texto-categoria cor-d1">Experiências</h2>
+      </div>
+      <div class="section-body texto-g cor-c7">
+        <div class="linha">
+          <p>Analista Sênior de <strong>SEO técnico</strong> aprendendo mais sobre o mundo de desenolvimento <strong>front end.</strong></p>
+        </div>
+        <ul class="experiencias-cards">
+
+          <li class="experiencias-card-item">
+            <div class="experiencias-meta">
+              <div class="experiencias-meta-empresa">
+                <h3 class="texto-g-b cor-c7">Creditas</h3>
+                <p class="texto-g-b cor-c4">Analista de SEO Sr.</p>
+              </div>
+              <div class="texto-m cor-c4 experiencias-meta-ano">
+                <p>2020 - atual</p>
+              </div>
+            </div>
+            <div class="texto-m cor-c7 experiencias-content">
+              <p>Responsável pelo SEO Técnico da companhia. Trabalho lado a lado diariamente com times de desenvolvimento e tecnologia para entregas as melhores soluções. Foco em performance, core web vitals e rastreabilidade.</p>
+            </div>
+          </li>
+
+          <li class="experiencias-card-item">
+            <div class="experiencias-meta">
+              <div class="experiencias-meta-empresa">
+                <h3 class="texto-g-b cor-c7">Consultoria Digital</h3>
+                <p class="texto-g-b cor-c4">Analista de Marketing</p>
+              </div>
+              <div class="texto-m cor-c4 experiencias-meta-ano">
+                <p>2017 - 2020</p>
+              </div>
+            </div>
+            <div class="texto-m cor-c7 experiencias-content">
+              <p>Responsável por toda estratégia digital de uma carteira de clientes composta dos mais variados segmentos, como sites institucionais B2B, blogs culinários, moda, e-commerces variados, plataformas de vendas de cursos e etc.</p>
+            </div>
+          </li>
+
+          <li class="experiencias-card-item">
+            <div class="experiencias-meta">
+              <div class="experiencias-meta-empresa">
+                <h3 class="texto-g-b cor-c7">Consultoria Digital</h3>
+                <p class="texto-g-b cor-c4">Analista de SEO</p>
+              </div>
+              <div class="texto-m cor-c4 experiencias-meta-ano">
+                <p>2016 - 2017</p>
+              </div>
+            </div>
+            <div class="texto-m cor-c7 experiencias-content">
+              <p>Responsável pelo acompanhamento de resultados, definição de estratégias, acompanhamento de tendências, auditorias e tudo que envolvia as estratégias de SEO dos meus clientes.</p>
+            </div>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </section>
+
+  <section id="formacao" class="dark-bg padding">
+    <div class="container formacao">
+      <div class="section-title">
+        <h2 class="texto-categoria cor-d1">Formação</h2>
+      </div>
+      <div class="section-body">
+        <ul class="formacao-cards">
+          <li class="formacao-cards-item">
+            <h3 class="texto-m-2 cor-c7 nome">Análise e Desenvolvimento de Sistemas</h3>
+            <p class="texto-m cor-c5 local">Universidade Estácio de Sá</p>
+            <p class="texto-g-b cor-c6 tipo">Graduação</p>
+          </li>
+          <li class="formacao-cards-item">
+            <div class="formacao-cards-item-meta">
+              <h3 class="texto-m-2 cor-c7 nome">Análise e Desenvolvimento de Sistemas</h3>
+              <p class="texto-m cor-c5 local">Fundação Instituto Tecnológico de Osasco</p>
+            </div>
+            <p class="texto-g-b cor-c6 tipo">Ensino Técnico</p>
+          </li>
+        </ul>
+        <div class="formacao-cursos">
+          <h3 class="texto-g-b cor-c1">Cursos</h3>
+          <ul class="texto-m cor-c2">
+            <li class="andamento">UI/UX Design e Front End - Origamid</li>
+            <li class="andamento">Fullstack - B7Web</li>
+            <li class="concluido">Curso de SEO - Consultoria Digital</li>
+            <li class="concluido">Checklist SEO - Rafael Rez</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contato" class="padding">
+    <div class="contato container">
+      <div class="section-title">
+        <h2 class="texto-categoria cor-d1">Fale comigo</h2>
+      </div>
+      <div class="section-body">
+        <p class="texto-xg-b cor-c7">Caso queira falar comigo, pode me mandar um e-mail que te respondo ;)</p>
+        <p class="texto-xg cor-c7"><a href="mailto:samuelpeixotovieira@gmail.com">samuelpeixotovieira<wbr>@gmail.com</a></p>
+        <div class="hero-info">
+          <div class="hero-info-icons">
+            <a href="http://instagra.com/speixoto_" target="_blank"><img src="./img/instagram.svg" alt="instagram" class="hero-info-icon"></a>
+            <a href="https://www.linkedin.com/in/samuel-p-vieira/" target="_blank"><img src="./img/linkedin.svg" alt="linkedin" class="hero-info-icon"></a>
+            <a href="https://twitter.com/speixoto_" target="_blank"><img src="./img/twitter.svg" alt="Twiter" class="hero-info-icon"></a>
+            <a href="https://github.com/smkvieira" target="_blank"><img src="./img/github.svg" alt="github" class="hero-info-icon"></a>
+            <a href="https://dribbble.com/smkvieira" target="_blank"><img src="./img/dribble.svg" alt="dribble" class="hero-info-icon"></a>
+            <a href="https://www.behance.net/speixoto_" target="_blank"><img src="./img/behance.svg" alt="behance" class="hero-info-icon"></a>
+          </div>
+        </div>
+      </div>
+  </section>
+
+  <footer class="dark-bg">
+    <div class="footer container">
+      <span class="texto-branding cor-c1">Samuel Vieira</span>
+      <span class="texto-g cor-c1">Todos os direitos reservados.</span>
+    </div>
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
