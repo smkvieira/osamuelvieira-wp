25d07dff295afb5f2be9d57cb2f4e8098c0c4819
Samuel Vieira <smlpeixotovieira@icloud.com>
2022-01-05 08:05:19 -0300

    Header e Footer php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="dark-bg">
+    <div class="footer container">
+      <span class="texto-branding cor-c1">Samuel Vieira</span>
+      <span class="texto-g cor-c1">Todos os direitos reservados.</span>
+    </div>
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Samuel Vieira</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+
+  <link rel="icon" type="image/png" href=".<?php echo get_stylesheet_directory_uri() ?>/img/favicon.png" />
+</head>
+
+<body>
+  <header>
+    <nav>
+      <div class="navbar container">
+        <div class="texto-branding cor-c9">Samuel Vieira</div>
+        <div class="navbar-menu">
+          <ul class="texto-m">
+            <li><a href="#experiencias">Experiência</a></li>
+            <li><a href="#formacao">Formação</a></li>
+            <li><a href="#contato">Contato</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
