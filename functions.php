37fb806606ffa8d5a9dbb6fc242f37dc15f6a0f1
Samuel Vieira <smlpeixotovieira@icloud.com>
2022-01-06 10:02:57 -0300

    Página de listagem de posts

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+    add_theme_support('post-thumbnails');
+
+    //Limitando o tamanho do excerpt
+    function custom_excerpt_length( $length ) {
+        return 30;
+    }
+    add_filter( 'excerpt_length', 'custom_excerpt_length');
+
     /***********************************************************************
     *                                                                      *
     *                                                                      *
--- home.php
+++ home.php
@@ -4,8 +4,21 @@ Usando Home
 <section>
     <div class="container corpo-pagina">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="titulo-pagina">
-        <h1 class="texto-xg cor-c9"><?php the_title(); ?></h1>
+    <div class="post-container">
+        <div class="post-img-destaque">
+            <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+                <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'medium' ); ?>">
+            </a>
+        </div>
+        <div class="post-content">
+            <div class="post-meta-info">
+                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
+                    <h1 class="post-titulo texto-g cor-c9"><?php the_title(); ?></h1>
+                </a>
+                <p class="post-data texto-s cor-c7">Publicado em <?php echo get_the_date(); ?></p>
+                <p class="post-excerpt texto-m cor-c7"><?php the_excerpt(); ?></p>
+            </div>
+        </div>
     </div>
     
     <?php endwhile; else: ?>
